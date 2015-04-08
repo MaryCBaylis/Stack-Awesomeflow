b512d9a74640f75d3b2ca5636d0f0ea404503cee
Mary Baylis <mary.c.baylis@gmail.com>
2015-04-08 17:42:52 -0400

    Add db seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Question.destroy_all
+Answer.destroy_all
+Response.destroy_all
+Vote.destroy_all
+
+20.times do
+	User.create!(email: Faker::Internet.email, password: "catcatcat")
+end
+
+50.times do
+	Question.create!(user: User.all.sample, title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+end
+
+200.times do
+	Answer.create!(user: User.all.sample, body: Faker::Lorem.paragraph, question: Question.all.sample)
+end
+
+Question.all.each do |q|
+	q.answers.sample.is_best = true unless q.answers.empty?
+end
+
+200.times do
+	Response.create!(user: User.all.sample, body: Faker::Lorem.sentence, post: Question.all.sample)
+end
+
+500.times do
+	Response.create!(user: User.all.sample, body: Faker::Lorem.sentence, post: Answer.all.sample)
+end
+
+2000.times do
+	Vote.create!(value: 1, user: User.all.sample, votable: Question.all.sample)	
+end
+
+1000.times do
+	Vote.create!(value: 1, user: User.all.sample, votable: Answer.all.sample)	
+end
+
+1000.times do
+	Vote.create!(value: 1, user: User.all.sample, votable: Response.all.sample)	
+end
+
+200.times do
+	Vote.create!(value: -1, user: User.all.sample, votable: Question.all.sample)	
+end
+
+100.times do
+	Vote.create!(value: -1, user: User.all.sample, votable: Answer.all.sample)	
+end
+
+50.times do
+	Vote.create!(value: -1, user: User.all.sample, votable: Response.all.sample)	
+end

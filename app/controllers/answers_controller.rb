5eb3aafa2665ad246de8eee5db3ca72bd417e253
Mary Baylis <mary.c.baylis@gmail.com>
2015-04-09 16:11:38 -0400

    Add set_question and set_answer methods to answers controller.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,14 @@
 class AnswersController < ApplicationController
 
+  before_action :set_question
+  before_action :set_answer, only: [:edit, :update, :destroy]
+
   def new
     @answer = Answer.new
-    @question = Question.find_by(id: params[:question_id])
   end
 
   def create
     user = User.find_by(id: current_user.id)
-    @question = Question.find_by(id: params[:question_id])
     @answer = Answer.new(question: @question, body: params[:answer][:body], user: user)
     if (@answer.save)
       redirect_to question_path(@question)
@@ -17,13 +18,9 @@ class AnswersController < ApplicationController
   end
 
   def edit
-    @question = Question.find_by(id: params[:question_id])
-    @answer = Answer.find_by(id: params[:id])
   end
 
   def update
-    @question = Question.find_by(id: params[:question_id])
-    @answer = Answer.find_by(id: params[:id])
     if @answer.user_id == current_user.id
       if @answer.update(body: params[:answer][:body])
         redirect_to question_path(@question)
@@ -36,15 +33,22 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    @question = Question.find_by(id: params[:question_id]
-      )
-    @answer = Answer.find_by(id: params[:id])
     if @answer.user_id == current_user.id
       @answer.destroy
       redirect_to questions_path, alert: "You successfully deleted your answer."
     else
       redirect_to questions_path, alert: "That was bad, and you should feel bad.  You're bad."
     end
+  end
+
+  protected
+
+  def set_question
+    @question = Question.find_by(id: params[:question_id])
+  end
+
+  def set_answer
+    @answer = Answer.find_by(id: params[:id])
   end
 
   private

ea1209aacb21bbc62744d3944554c638d7ca6092
Chris Singer <ccsinger@gmail.com>
2015-04-09 18:58:44 -0400

    Add route and controller actions for marking answer as best.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :questions do
+    get 'mark_best', on: :member
     resources :answers, only: [:new, :edit, :destroy, :create, :update]
   end
   

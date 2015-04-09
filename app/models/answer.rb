aef66031697aad02b7a988be076d4fbb407f3971
Chris Singer <ccsinger@gmail.com>
2015-04-09 18:14:32 -0400

    Update associations to relationship instead of ID.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,10 @@
 class Answer < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :question
+  belongs_to :user, inverse_of: :answers
+  belongs_to :question, inverse_of: :answers
   has_many :responses, as: :post, dependent: :destroy
   has_many :votes, as: :votable, dependent: :destroy
 
   validates :body, presence: true
-  validates :user_id, presence: true
-  validates :question_id, presence: true
+  validates :user, presence: true
+  validates :question, presence: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,8 @@
 class Vote < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :votable, polymorphic: true
+  belongs_to :user, inverse_of: :votes
+  belongs_to :votable, polymorphic: true, inverse_of: :votes
 
   validates :value, presence: true, inclusion: {in: [-1, 1]} 
-  validates :user_id, presence: true
-  validates :votable_id, presence: true
+  validates :user, presence: true
+  validates :votable, presence: true
 end

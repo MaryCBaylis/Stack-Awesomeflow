9512c6f75a8e6b2575d704c9c887199c88d1d4a4
Peter Mueller <peter.kh.mueller@gmail.com>
2015-04-09 10:31:29 -0400

    add index with link to question with title, user, and time

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,43 +11,43 @@ class QuestionsController < ApplicationController
     end
   end
 
-	def new
-		@question = Question.new
-	end
+  def new
+    @question = Question.new
+  end
 
-	def create
-		params[:question][:user_id] = current_user.id
-		@question = Question.new(question_params)
+  def create
+    params[:question][:user_id] = current_user.id
+    @question = Question.new(question_params)
 
-		if @question.save
-			redirect_to question_path(@question)
-		else
-			render "new"
-		end
-	end
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render "new"
+    end
+  end
 
-	def edit
-		@question = Question.find_by(id: params[:id])
-	end
+  def edit
+    @question = Question.find_by(id: params[:id])
+  end
 
-	def update
-		@question = Question.find_by(id: params[:id])
-		if @question && @question.update(question_params)
-			redirect_to question_path(@question)
-		else
-			render "edit"
-		end
-	end
+  def update
+    @question = Question.find_by(id: params[:id])
+    if @question && @question.update(question_params)
+      redirect_to question_path(@question)
+    else
+      render "edit"
+    end
+  end
 
-	def destroy 
-		@question = Question.find_by(id: params[:id])
-		@question.destroy
-		redirect_to questions_path
-	end 
+  def destroy
+    @question = Question.find_by(id: params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
 
-	private
+  private
 
-	def question_params
-		params.require(:question).permit(:title, :body, :user_id)
-	end
+  def question_params
+    params.require(:question).permit(:title, :body, :user_id)
+  end
 end

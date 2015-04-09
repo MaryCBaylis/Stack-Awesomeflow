f1a2bb4c8368d545c2083a9f7b7a3a22b94e40ba
Mary Baylis <mary.c.baylis@gmail.com>
2015-04-09 15:34:50 -0400

    Updated allowed routes for answers.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :questions do
-    resources :answers
+    resources :answers, only: [:new, :edit, :destroy, :create, :update]
   end
   
 end

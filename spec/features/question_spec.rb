90db6ad96efc0f3270332a110b6d105a5a8dc527
msomji <maisam.somji@gmail.com>
2015-04-09 18:12:42 -0400

    added update question feature test

---
+++ spec/features/question_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+
+feature "User viewing a question page" do
+  let(:user) { User.create!(email: "bob@boby.com", password: "12341234")  }
+  let!(:question) { Question.create!(title: "titletitle", body: "this is the body", user: user)}
+
+  before do
+    visit new_user_session_path
+    find('#user_email').set(user.email)
+    find('#user_password').set(user.password)
+    click_button('Log in')
+  end
+
+
+  scenario "when user views a question" do
+    # when
+    visit question_path(question)
+    # then
+    expect(page).to have_content(question.title)
+
+  end
+
+  scenario "When user tries to update their question, text field are prefilled" do
+    visit edit_question_path(question)
+    
+    expect(page.find("textarea").text).to eq(question.body)
+
+  end
+
+  scenario "when user tries to delete their question" do
+
+  end
+
+  
+end
